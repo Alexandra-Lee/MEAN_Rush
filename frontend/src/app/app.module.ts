8f4982342751027edce449806e07bd55a31aaaa0
Alexandra LEE <palexalee@gmail.com>
2018-09-10 01:10:27 +0100

    "added components"

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostsComponent } from './components/posts/posts.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'posts', pathMatch: 'full' },
+  { path: 'create', component: CreateComponent },
+  { path: 'edit/:id', component: EditComponent },
+  { path: 'posts', component: PostsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    CreateComponent,
+    EditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
